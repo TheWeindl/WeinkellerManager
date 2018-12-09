0da5f1e9dd993cf59538ec35c9421fe281822fa8
TheWeindl <theweindl@gmail.com>
2018-12-09 21:50:53 +0100

    Started with bottle adding form

---
+++ js/add.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Florian on 09.12.2018.
+ */
+
+$(document).ready(function () {
+
+    $('#select-type').selectize({
+        placeholder: "Test",
+        sortField: "text",
+        create: true,
+        maxItems: 1,
+        options: [  {value: "redWine", text: "Rotwein"},
+                    {value: "whiteWine", text: "Weißwein"}]
+    });
+});
+
+$(document).on("click", ".addBtn" ,function (e) {
+
+    $.ajax({
+        url: "../php/Weinkeller.php",
+        data: {
+            q: "addBottle",
+            type: document.getElementById("type").value,
+            name: document.getElementById("name").value,
+            year: document.getElementById("year").value,
+            country: document.getElementById("country").value,
+            region: document.getElementById("region").value,
+            winery: document.getElementById("winery").value,
+            amount: document.getElementById("amount").value
+        },
+        type: "POST",
+        success: function () {
+            $(".place#f-" + shelf + " i").removeClass("fas red white").addClass("far");
+            $(".place#f-" + shelf + "").removeClass("taken");
+        }
+    });
+});
